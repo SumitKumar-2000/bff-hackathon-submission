ce346d2903ca66f03bb0b72fa7ea8eb9b793cf9f
sumitkumar <sumitkumarracer@gmail.com>
2023-04-02 00:13:01 +0530

    feed page route added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import FoodScan from "./pages/FoodScan"
 import Navbar from './components/Navbar/Navbar'
 import { useState } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import FeedPage from "./pages/FeedPage"
 
 function App() {  
 
@@ -12,6 +13,7 @@ function App() {
     <Router>
       <Navbar scan={scan} setScan={setScan}/>
       <Routes>
+        <Route path="/" element={<FeedPage/>} />
         <Route path="/foodScan" element={<FoodScan scan={scan} setScan={setScan} />} exact/> 
       </Routes>
     </Router>  
